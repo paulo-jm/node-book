711ef1b15b79fdc1f691bd6abf37f5cc6dec2ada
MoreiraP <MoreiraP@BR-MoreiraP.lac.uis.unisys.com>
2015-05-15 18:08:44 -0300

    add package cap-01

---
+++ node-book/public_html/cap-01/hello-word/hello.js
@@ -0,0 +1,13 @@
+
+
+http = require("http");
+
+http.createServer(function(request, response){
+    
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.end("hello word");
+    
+}).listen(1337, '127.0.0.1');
+
+console.log("Server running at http://127.0.0.1:1337/");
+
